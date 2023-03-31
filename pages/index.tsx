5f98414dc8b0b413034b95dff922960fc4badfa8
Indrajit Pal <palindrajit10@gmail.com>
2023-03-31 13:26:57 +0530

    nerflix clone

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import { signOut } from "next-auth/react"
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
+import Navbar from "@/components/navbar";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
+
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    </>
+  )
+}
